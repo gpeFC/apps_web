83578acf7fed0d3d199cf98fab77fd4f4b536eef
emanuel <emanuel_gp@hotmail.com>
2013-11-07 06:12:36 -0600

    presentacion_maquetada del sitio, nuevo proyecto(AppsWeb)

---
+++ project_final/multiserviciosmx/principal/views.py
@@ -0,0 +1,11 @@
+# Create your views here.
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+
+# Vista "Inicio".
+def inicio(request):
+    return render_to_response('inicio.html')
